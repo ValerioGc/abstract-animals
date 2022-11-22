d0191db3a811b9e2d23f0d1c27e05f00f7a5d570
ValerioGc <valerio.gc1@gmail.com>
2022-11-22 15:04:04 +0100

    Esercizio 1

--- src/org/office/italy/Main.java
+++ src/org/office/italy/Main.java
@@ -3,12 +3,12 @@ package org.office.italy;
 public class Main {
 
 	public static void main(String[] args) {
-		Employee ey1 = new Employee("Valerio", "Genco", "1997-12-17", 100, 12);
-		Employee ey2 = new Employee("Alessio", "esposito", "1997-12-17", 100, 12);
-		Employee ey3 = new Employee("Jacopo", "Bianchi", "1997-12-17", 100, 12);
+		Employee ey1 = new Employee("Valerio", "Genco", "1997-12-17", 2000, 12);
+		Employee ey2 = new Employee("Alessio", "esposito", "1997-12-17", 4000, 12);
+		Employee ey3 = new Employee("Jacopo", "Bianchi", "1997-12-17", 1200, 12);
 		
-		Employee bs1 = new Employee("Luca", "Verdi", "1997-12-17", 100, 3000);
-		Employee bs2 = new Employee("Mario", "Rossi", "1997-12-17", 100, 500);
+		Employee bs1 = new Employee("Luca", "Verdi", "1997-12-17", 3000, 600);
+		Employee bs2 = new Employee("Mario", "Rossi", "1997-12-17", 5000, 500);
 	
 	
 		Person[] agencyMembers = {ey1, ey2, ey3, bs1, bs2};
@@ -17,14 +17,14 @@ public class Main {
 		int minYIncome = Integer.MAX_VALUE;
 		Person mostPayed = null;
 		Person lessPayed = null;
-		maxCost
 		for (int i = 0; i < agencyMembers.length; i++) {
 			Person currentEmployee = agencyMembers[i];
-			
-			if (currentEmployee.getYearIncome() < yearIncome) {
-				maxYIncome = yearIncome;
+			int actualMaxIncome = currentEmployee.getYearIncome();
+			if (actualMaxIncome > maxYIncome) {
+				maxYIncome = actualMaxIncome;
 				mostPayed = currentEmployee;
 			}
 		}
+		System.out.println(mostPayed);
 	}
 }
